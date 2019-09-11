2a48f4820a80d0052935f3efb6ffb676d828dcb4
joostvannieu <joost.vannieuwenhuyse@gmail.com>
2019-09-11 10:03:22 +0200

    Commit of weather api all required data aggregation written in functions Up next, adding it to the template

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@
 
     /*
     * FUNCTIONS
+    * Adjust so it shows current stats for today, rather than averages
     * */
     //Get Avg Temp
     function getAvgTemp(dayBeingProcessed){
@@ -37,24 +38,77 @@
         });
         return maxTemp;
     }
+    //Find most occurring element in an array
+    function findMostOccurring(array){
+        let result,  // This is the value we will return
+            best = -1,   // Initialize the comparison for greedy
+            lookup = {}; // Hashmap for essentially constant lookup   // Loop to go through array and count elements
+        for(let i = 0; i < array.length; i++){
+            // could replace block with `x[a[i]] = (x[a[i]] + 1)|0`
+            //but far less readable
+            if (lookup[array[i]] === undefined){
+                lookup[array[i]] = 0;
+            }
+            lookup[array[i]]++; // Increment count
+            if(lookup[array[i]] > best){ // Greedy for best element
+                best = lookup[array[i]];
+                result = array[i]
+            }
+        }   //will return most frequent item in array
+        return result;
+    }
     //Get Weather condition + icon
     function getWeatherCondition(dayBeingProcessed){
-        let description, icon;
-        let condition = {
+        let descriptions = [], icons = [];
+        let weatherCondition = {
             description: 'desctest',
             icon: 'icontest'
         };
-        /*dayBeingProcessed.forEach(function (timeslot){
-            if (timeslot.main.temp_max > maxTemp)
-                maxTemp = Math.round(timeslot.main.temp_max);
-        });*/
-        return condition;
+        dayBeingProcessed.forEach(function (timeslot){
+            descriptions.push(timeslot.weather[0].description);
+            icons.push(timeslot.weather[0].icon.substr(0, 2));
+        });
+        weatherCondition.description = findMostOccurring(descriptions);
+        weatherCondition.icon = findMostOccurring(icons) + "d";
+        //console.log(descriptions);
+        //console.log(icons);
+        //console.log(weatherCondition);
+
+        return weatherCondition;
     }
     //Get Wind
+    function getAvgWind(dayBeingProcessed){
+        let tempsum = 0;
+        dayBeingProcessed.forEach(function (timeslot){
+            //console.log(timeslot.wind.speed);
+            tempsum += timeslot.wind.speed;
+        });
+        return Math.round(tempsum / dayBeingProcessed.length);
+    }
     //Get ...
+
+    //Console log all aggregated data
+    function consoleLogData(dayBeingProcessed){
+        console.log('NEW DAY');
+        console.log('Day being processed:');
+        console.log(dayBeingProcessed);
+        console.log('Avg Temp: ' + getAvgTemp(dayBeingProcessed) + '°C');
+        console.log('Min Temp: ' + getMinTemp(dayBeingProcessed) + '°C');
+        console.log('Max Temp: ' + getMaxTemp(dayBeingProcessed) + '°C');
+        console.log('Weather condition:');
+        console.log(getWeatherCondition(dayBeingProcessed));
+        console.log('Wind speed: ' + getAvgWind(dayBeingProcessed) + ' m/s');
+    }
+
+    //Fill the template and append it to the HTML DOM
+    function fillTemplate(dayBeingProcessed){
+
+    }
 
     /*
     * MAIN
+    * There seems to be an error at around midnight, where it cannot find info on the current day in the Json file
+    * This needs fixing somehow
     * */
     fetch(fetchString)
         .then(function (response) {
@@ -67,7 +121,6 @@
             //console.log(date.getDay());
 
             for(let i = 0; i < DAYS_IN_FORECAST; i++){
-                console.log('New Day');
                 let checkingDay;
                 let date; //= new Date(forecast[5].dt_txt);
                 let dayBeingProcessed = [];
@@ -82,12 +135,9 @@
                         dayBeingProcessed.push(dataPoint);
                     }
                 });
-                console.log(dayBeingProcessed);
 
-                console.log('Avg Temp: ' + getAvgTemp(dayBeingProcessed) + '°C');
-                console.log('Min Temp: ' + getMinTemp(dayBeingProcessed) + '°C');
-                console.log('Max Temp: ' + getMaxTemp(dayBeingProcessed) + '°C');
-                console.log(getWeatherCondition(dayBeingProcessed).description + ' ' + getWeatherCondition(dayBeingProcessed).icon);
+                //Show aggregated data in console
+                consoleLogData(dayBeingProcessed);
 
 
             }
